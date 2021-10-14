40365a1c808bc2c228d2a9d08a66d31780bac351
Yash-1305yg <yash.gupta_cs19@gla.ac.in>
2021-10-14 09:38:15 +0530

    first

---
+++ java.js
@@ -0,0 +1,10 @@
+function clickevent(){
+    alert("clicked");
+    console.log("yash");
+}
+
+function mousehover(){
+    var x= document.getElementById("color");
+    x =document.getElementById("color").backgroundColor="yellow";
+    console.log("gupta");
+}
